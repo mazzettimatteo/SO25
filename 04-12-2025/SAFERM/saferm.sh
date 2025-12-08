31308452986679710480ec244390935e0089aaf0
mazzettimatteo <mazzetti.matteo.ita@gmail.com>
2025-12-08 12:51:28 +0100

    saferm quasi finito
    Manca gestione file stesso nome

--- 04-12-2025/SAFERM/saferm.sh
+++ 04-12-2025/SAFERM/saferm.sh
@@ -21,15 +21,83 @@
 
 TRASH_DIR=trashForSAFERM/
 RIPRISTINATI=fileRipristinati/
+META_FILE=trashForSAFERM/.supportFile
+optCont=0
+
+
+mkdir -p $TRASH_DIR
+mkdir -p $RIPRISTINATI
+touch $META_FILE
+
+countOption(){
+	if [[ "$1" == "-P" || "$1" == "-R" || "$1" == "-L" ]]; then
+		((optCont++))
+	fi
+	if [[ "$2" == "-P" || "$2" == "-R" || "$2" == "-L" ]]; then
+		((optCont++))
+	fi 
+
+	if [[ $optCont -ge 2 ]]; then
+		echo "error: multiple options"
+		exit 1
+	fi
+}
+
+
+restoreFile(){
+	local file="$1"
+	local fileNoDots=$(basename "$file")
+	
+	#echo "$fileNoDots"
+	if [[ -z "$fileNoDots" ]]; then
+		echo "error: inserire nome di un file"
+		return 1
+	fi
+	
+	if [[ -f "$TRASH_DIR/$fileNoDots" ]]; then
+
+		local originalPath=$(grep "$file|" "$META_FILE" | cut -d'|' -f2)
+		originalPath=$(realpath "$originalPath")
+		#echo "$originalPath"
+		#grep "pattern" "where" 
+		# -d'|' dice che il delimitatore del cut è la "|" e -f2 gli dice di prendere la seconda parte del taglio
+
+		mv "$TRASH_DIR/$fileNoDots" "$originalPath"
+		echo "File $file ripristinato"
+	else
+		echo "error il file non è in $TRASH_DIR"
+	fi
+}
+
+removeFile(){
+	local file="$1"
+	file=($basefile "$file")
+	local fullPath=$(pwd)/"$file"
+	if [[ -z "$fullPath" ]]; then
+		echo "error: inserire file esistente"
+		return 1
+	else
+		mv $fullPath $TRASH_DIR
+		echo "$file|$fullPath" >> "$META_FILE"  
+	fi
+}
 
 #----GESTIONE PARAM--------
+
+countOption "$1" "$2" #if there are multiple options eg saferm -L -P quits
+
 case $1 in
-	"-L") ls $TRASH_DIR
+	"-L") 
+		ls $TRASH_DIR #list trash content
 	;;
-	"-P") rm $TRASH_DIR*
+	"-P") 
+		rm $TRASH_DIR* #purge tash bin
 	;;
-	"-R")  
-		mv $TRASH_DIR$2 $RIPRISINATI
+	"-R")  #restore file
+		restoreFile "$2"
+	;;
+	*)
+		removeFile "$1"
 	;;
 esac
 
